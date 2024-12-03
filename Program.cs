ae92e621691c2211e0ad8f9182c81e2b703cccaa
Par0lev-K1LLer2005 <kirill.loginov1995@mail.ru>
2024-12-03 17:54:18 +0500

    Statistic GET stat

--- Program.cs
+++ Program.cs
@@ -43,6 +43,27 @@ app.MapGet("/update", ([AsParameters] UpdateOrderDTO dto) =>
         order.Comments.Add(dto.Comment);
 });
 
+int complete_count() => repo.FindAll(x => x.Status == "выполнено").Count;
+
+Dictionary<string, int> get_problem_type_stat() =>
+    repo.GroupBy(x => x.ProblemType)
+    .Select(x => (x.Key, x.Count()))
+    .ToDictionary(k => k.Key, v => v.Item2);
+
+double get_average_time_to_complete() =>
+    complete_count() == 0 ? 0 :
+    repo.FindAll(x => x.Status == "выполнено")
+    .Select(x => x.EndDate.Value.DayNumber - x.StartDate.DayNumber)
+    .Sum() / complete_count();
+
+
+app.MapGet("/stat", () => new
+{
+    complete_count = complete_count(),
+    get_problem_type_stat = get_problem_type_stat(),
+    get_average_time_to_complete = get_average_time_to_complete()
+});
+
 app.Run();
 
 
