3ae14a8d8a84a8762320f62cb324e36a13f2d50f
Par0lev-K1LLer2005 <kirill.loginov1995@mail.ru>
2024-12-03 17:10:59 +0500

    Class Order, DTO, List

---
+++ Program.cs
@@ -0,0 +1,29 @@
+List<Order> repo = [
+    new Order(1,new(2005,03,03),"микроволновкка", "коротнуло", "вставил в розетку и всё", "Игорь", "в ожидании" ),
+
+    ];
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/orders", () => repo );
+
+app.Run();
+
+
+class Order (int number, DateOnly startDate, string device, string problemType, string description, string client, string status )
+{
+    public int Number { get; set; } = number;
+    public DateOnly StartDate { get; set; } = startDate;
+    public string Device { get; set; } = device;
+    public string ProblemType { get; set; } = problemType;
+    public string Description { get; set; } = description;
+    public string Client { get; set; } = client;
+    public string Status { get; set; } = status;
+    public DateOnly? EndDate { get; set; } = null;
+    public string? Master { get; set; } = "не назначен";
+    public List<string> Comments { get; set; } = [];
+
+}
+
+record class UpdateOrderDTO(int Number, string? Status = "", string? Description = "", string? Master = "", string? Comment = "" );
